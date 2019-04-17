47ec7b162c4112cef73735b24ac783fbb97b505c
carlos <xsolrac87@gmail.com>
2019-04-17 23:35:45 +0200

    Complete

--- src/App.js
+++ src/App.js
@@ -1,138 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
-// Import our Person component
-import Person from './Person/Person';
+import Validator from './Validator/Validator';
+import Char from './Char/Char';
 
 class App extends Component {
 
-  // state is a special property only availabe within a class that extends Component
-  // in the later versions of react it's possible to use it in components functions
   state = {
-    persons: [
-      { name: 'Carlos', age: 30 },
-      { name: 'Paco', age: 50 },
-      { name: 'Foo', age: 10 }
-    ]
+    userInput: ''
   }
 
-  switchNameHandler = newName => {
-    // DONT DO THIS this.state.persons[0].name = "JHON DOE";
-    // to change the state property we can use the method setState include in the Component
-    this.setState({
-      persons: [
-        { name: newName, age: 80 },
-        { name: 'Jhon Doe', age: 66 },
-        { name: 'Foo Foo', age: 16 }
-      ]
-    });
+  inputListenerHandler = (event) => {
+    this.setState({ userInput: event.target.value })
   }
 
-  nameChangeHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Carlos XX", age: 80 },
-        { name: event.target.value, age: 66 },
-        { name: 'Foo Foo', age: 16 }
-      ]
-    });
+  removeCharacterHandler = (index) => {
+    // split the string into array 
+    let text = this.state.userInput.split('');
+    text.splice(index, 1);
+
+    // convert the array to string again
+    const userInputUpdated = text.join('');
+    this.setState({userInput: userInputUpdated});
   }
 
   render() {
-    // use css style a js object
-    let styleObj = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      boder: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-    
+
+    const charList = this.state.userInput.split('').map((char, index) => {
+      return <Char
+        character={char}
+        clicked={() => this.removeCharacterHandler(index)}
+        key={index} />
+    });
+
+    const userInputLength = this.state.userInput.length;
+
     return (
-      <div className='App'>
-        <h1>Hi, from the react app!</h1>
-        <p>I'm running from a docker container!</p>
+      <div className="App">
+        <ol>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+        </ol>
+        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
 
-        {/* calling the function without (), because calling it with switchNameHandler()
-         will render automatically on DOM render */}
-        <button
-        style={styleObj} 
-        onClick={this.switchNameHandler.bind(this, "Carlos IX")}>Switch Names</button>
+        <hr />
+        <input
+          type="text"
+          onChange={this.inputListenerHandler}
+          value={this.state.userInput} />
 
-        {/* another way to call the function with arguments is using flat arrow function */}
-        {/* this one is more inefficient may impact in performance */}
-        {/* <button onClick={() => this.switchNameHandler("King Carlos II")}>Switch Names</button> */}
+        <p>{userInputLength}</p>
 
-        {/* Use the component that we just have imported */}
-        {/* Pass variables value to the props in the function Person */}
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        {/* To use the text between the component we'll use the props.children in the function */}
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Amateratsu")}
-          change={this.nameChangeHandler}>My hobbies: racing </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+        <Validator
+          length={userInputLength} />
+
+        {charList}
       </div>
     );
   }
 }
 
 export default App;
-
-// How to use react Hook instead extends component
-// first import useState
-//import React, { useState } from 'react';
-
-// react Hooks
-// create function instead class
-// const app = props => {
-
-//   // setState from react Hook returns always two elements
-//   // the first element is always the current object
-//   // the second element is a function that allow us to update / change the state
-//   // using js Destructuring we cant store those values in an array
-//   const [personState, setPersonState] = useState({
-//     persons: [
-//       { name: 'Carlos', age: 30 },
-//       { name: 'Paco', age: 50 },
-//       { name: 'Foo', age: 10 }
-//     ]
-//   });
-
-//   // when we use react hook the function ( in this cade setPersonState ), doest not merge the changes
-//   // so we need to call useState again, we dont need to pass an object, just a string will work too
-//   const [otherState, setOtherState] = useState("some random value");
-
-//   console.log(personState, otherState);
-
-//   const switchNameHandler = () => {
-//     setPersonState({
-//       persons: [
-//         { name: 'Carlos VII', age: 80 },
-//         { name: 'Jhon Doe', age: 66 },
-//         { name: 'Foo Foo', age: 16 }
-//       ]
-//     });
-//   }
-
-//   return (
-//     <div className='App'>
-//       <h1>Hi, from the react app!</h1>
-//       <p>I'm running from a docker container!</p>
-
-//       <button onClick={switchNameHandler}>Switch Names</button>
-
-//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-//       {/* To use the text between the component we'll use the props.children in the function */}
-//       <Person name={personState.persons[1].name} age={personState.persons[1].age}>My hobbies: racing </Person>
-//       <Person name={personState.persons[2].name} age={personState.persons[2].age} />
-//     </div>
-//   );
-// }
-
-// //export the function with the name of it
-// export default app;
